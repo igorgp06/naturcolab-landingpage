4a6268859d496598ca16ad92eb045a935276b88f
igorgp06 <contateme.igorgp@gmail.com>
2024-12-17 00:44:56 -0300

    conclusão da seção about e outras leves alterações

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,21 @@
+// menu
 document.addEventListener('DOMContentLoaded', function () {
-    const menuIcon = document.querySelector('.menu-icon');
-    const closeIcon = document.querySelector('.close-icon');
-    const menu = document.getElementById('menu');
+    const menuIcon = document.querySelector('.menu-icon')
+    const closeIcon = document.querySelector('.close-icon')
+    const menu = document.getElementById('menu')
+    const menuLinks = document.querySelectorAll('.navbar-list li a')
 
     menuIcon.addEventListener('click', function () {
-        menu.classList.add('open');
-    });
+        menu.classList.add('open')
+    })
 
     closeIcon.addEventListener('click', function () {
-        menu.classList.remove('open');
-    });
-});
+        menu.classList.remove('open')
+    })
+
+    menuLinks.forEach(link => {
+        link.addEventListener('click', function () {
+            menu.classList.remove('open')
+        })
+    })
+})
